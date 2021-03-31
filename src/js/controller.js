1bd3178b997f01251fa0a99a4467dd6952aa698f
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2021-03-31 10:26:14 +0530

    helpers and congif files created

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,18 +4,6 @@ import 'regenerator-runtime/runtime';
 import * as model from './model';
 import recipeView from './views/recipeView';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
-
 const controlRecipe = async function () {
   try {
     const id = window.location.hash.slice(1);
@@ -28,6 +16,7 @@ const controlRecipe = async function () {
 
     // 2. Rendering Recipe
     recipeView.render(model.state.recipe);
+    
   } catch (error) {
     alert(error);
   }
